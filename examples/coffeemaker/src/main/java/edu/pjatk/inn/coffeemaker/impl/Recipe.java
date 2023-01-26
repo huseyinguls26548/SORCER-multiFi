c17c70ae15e6d43d8b96457fa093486a007089d3
Urszula Baranowska <28218304+umbaranowska@users.noreply.github.com>
2023-01-26 06:47:36 +0100

    added Recipe.java with docstrings

--- examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/impl/Recipe.java
+++ examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/impl/Recipe.java
@@ -8,17 +8,26 @@ import java.io.Serializable;
 import java.rmi.RemoteException;
 
 /**
+ * The Recipe class defines a coffee drink recipe
+ * and allows to display and modify it when needed.
+ *
  * @author   Sarah & Mike
  */
 public class Recipe implements Serializable {
+	/**
+	 * Fields
+	 */
     private String name;
     private int price;
     private int amtCoffee;
     private int amtMilk;
     private int amtSugar;
     private int amtChocolate;
-    
-    public Recipe() {
+
+	/**
+	 * Constructor
+	 */
+	public Recipe() {
     	this.name = "";
     	this.price = 0;
     	this.amtCoffee = 0;
@@ -28,13 +37,17 @@ public class Recipe implements Serializable {
     }
     
     /**
-	 * @return   Returns the amtChocolate.
+	 * This method returns the amount of chocolate in the drink.
+	 *
+	 * @return The amount of chocolate in the drink.
 	 */
     public int getAmtChocolate() {
 		return amtChocolate;
 	}
     /**
-	 * @param amtChocolate   The amtChocolate to setValue.
+	 * This method modifies the amount of chocolate in the drink.
+	 *
+	 * @param amtChocolate The integer value of the new amount of chocolate in the drink.
 	 */
     public void setAmtChocolate(int amtChocolate) {
 		if (amtChocolate >= 0) {
@@ -42,13 +55,17 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
-	 * @return   Returns the amtCoffee.
+	 * This method returns the amount of coffee in the drink.
+	 *
+	 * @return The amount of coffee in the drink.
 	 */
     public int getAmtCoffee() {
 		return amtCoffee;
 	}
     /**
-	 * @param amtCoffee   The amtCoffee to setValue.
+	 * This method modfies the amount of coffee in the drink.
+	 *
+	 * @param amtCoffee The integer value of the new amount of coffee in the drink.
 	 */
     public void setAmtCoffee(int amtCoffee) {
 		if (amtCoffee >= 0) {
@@ -56,13 +73,17 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
-	 * @return   Returns the amtMilk.
+	 * This method returns the amount of coffee in the drink.
+	 *
+	 * @return The amount of milk in the drink.
 	 */
     public int getAmtMilk() {
 		return amtMilk;
 	}
     /**
-	 * @param amtMilk   The amtMilk to setValue.
+	 * This method modfies the amount of coffee in the drink.
+	 *
+	 * @param amtMilk The integer value of the new amount of milk in the drink.
 	 */
     public void setAmtMilk(int amtMilk) {
 		if (amtMilk >= 0) {
@@ -70,13 +91,17 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
-	 * @return   Returns the amtSugar.
+	 * This method returns the amount of sugar in the drink.
+	 *
+	 * @return The amount of milk in the drink.
 	 */
     public int getAmtSugar() {
 		return amtSugar;
 	}
     /**
-	 * @param amtSugar   The amtSugar to setValue.
+	 * This method modfies the amount of sugar in the drink.
+	 *
+	 * @param amtSugar The integer value of the new amount of sugar in the drink.
 	 */
     public void setAmtSugar(int amtSugar) {
 		if (amtSugar >= 0) {
@@ -84,13 +109,17 @@ public class Recipe implements Serializable {
 		} 
 	}
     /**
-	 * @return   Returns the key.
+	 * This method returns the name of the drink
+	 *
+	 * @return Returns the name of the drink.
 	 */
     public String getName() {
 		return name;
 	}
     /**
-	 * @param name   The key to setValue.
+	 * This method modifies the name of the drink.
+	 *
+	 * @param name The string to set the new drink name to.
 	 */
     public void setName(String name) {
     	if(name != null) {
@@ -98,13 +127,17 @@ public class Recipe implements Serializable {
     	}
 	}
     /**
-	 * @return   Returns the price.
+	 * This method returns the price of the drink.
+	 *
+	 * @return Returns the price of the drink.
 	 */
     public int getPrice() {
 		return price;
 	}
     /**
-	 * @param price   The price to setValue.
+	 * This method modifies the price of the drink.
+	 *
+	 * @param price The integer value to set the new price to.
 	 */
     public void setPrice(int price) {
 		if (price >= 0) {
